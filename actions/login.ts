cf784cc0ca4a47e7d8c221cfca5e5ab68006406e
Avishek Saha <avisheksaha@Mac.lan>
2026-05-03 10:40:28 +0530

    auth_init

--- actions/login.ts
+++ actions/login.ts
@@ -1,24 +1,56 @@
 "use server"
 
-import { LoginSchema } from "@/schemas";
-import * as z from "zod";
+import { signIn } from '@/auth';
+import { getUserByEmail } from '@/data';
+// import { generateVerificationToken } from '@/lib/tokens';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
+import * as z from 'zod';
 
-
-
-
-export const Login = async (values: z.infer<typeof LoginSchema>) => {
-   
+export const Login = async (values : z.infer<typeof LoginSchema>) => {
     const validatedInputs = LoginSchema.safeParse(values);
-    
     if(!validatedInputs.success){
         return {
-            error: "Invalid Format"
-        };
+            error: "Invalid inputs",
+        }
     }
-
     const { email,password } = validatedInputs.data;
-    return {
-        success: "Successfully logged in"
-    }
     
+    // const existingUser = await getUserByEmail(email);
+    // if(!existingUser || !existingUser.email || !existingUser.password){
+    //     return {
+    //         error: "Email doesn't exist!"
+    //     }
+    // }
+    // if(!existingUser.emailVerified){
+    //     const verificationToken = await generateVerificationToken(existingUser.email);
+    //     return {
+    //         success: "confirmation mail is sent!"
+    //     }
+    // }
+    try{
+     await signIn("credentials",{
+        email,
+        password,
+        redirectTo: DEFAULT_LOGIN_REDIRECT,
+     })
+    }catch(error){
+      if(error instanceof AuthError){
+        switch(error.type){
+            case "CredentialsSignin":
+                return {
+                    error: "Invalid credentials"
+                }
+            default:
+                 return {
+                    error: "Something went wrong!"
+                 }    
+        }
+      }
+      throw error
+    }
+    return {
+        success: "Email sent"
+    }
 }
--- components/auth/Socials.tsx
+++ components/auth/Socials.tsx
@@ -3,18 +3,24 @@
 import { Button } from "../ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 
 const Socials = () => {
    
-
+  const onClickHandler = (provider: "google" | "github") => {
+    signIn(provider,{
+        redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  }
 
     return (
      <div className="w-full flex justify-center gap-x-4">
         <Button variant={"secondary"} className="cursor-pointer">
             <FcGoogle className="w-5 h-5"/>
         </Button>
-        <Button variant={"secondary"} className="cursor-pointer">
+        <Button variant={"secondary"} className="cursor-pointer" onClick={() => {onClickHandler("github")}}>
             <FaGithub className="w-5 h-5"/>
         </Button>
      </div>
